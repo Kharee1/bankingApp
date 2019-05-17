da05f49c8c3c3037af469506ca2fc1edc7fcfdc5
Edwin Sanchez <es269@njit.edu>
2019-05-17 12:32:21 -0400

    home page account displays

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch ('http://my-json-server.typicode.com/kharee1/bankingApp/accounts')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          items: json,
+        })
+      });
+  }
+
+  render() {
+    var { items } = this.state;
+    return (
+      <div className="App">
+        <table>
+          {items.map(item => (
+            <tr key = {item.id}>
+              Name: {item.name} <td>Balance: {item.balance}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
+
+}
+
+export default App;
